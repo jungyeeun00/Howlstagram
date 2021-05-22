27a164f0f35f1322e330b6be3ac39bc31b213963
jungyeeun00 <ekfqlc0103@naver.com>
2021-05-22 12:36:39 +0900

    google login

--- app/src/main/java/com/example/howlstagram/LoginActivity.kt
+++ app/src/main/java/com/example/howlstagram/LoginActivity.kt
@@ -3,23 +3,76 @@ package com.example.howlstagram
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
+import com.example.howlstagram.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import kotlinx.android.synthetic.main.activity_login.*
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityLoginBinding
+
     var auth : FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         auth = FirebaseAuth.getInstance()
-        email_login_button.setOnClickListener {
+        binding.emailLoginButton.setOnClickListener {
             signinAndSignup()
         }
+        binding.googleSignInButton.setOnClickListener {
+            //First step
+            googleLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result != null) {
+                if (result.isSuccess){
+                    var account = result.signInAccount
+                    //Second step
+                    firebaseAuthWithGoogle(account)
+                }
+            }
+        }
+    }
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                    task->
+                if (task.isSuccessful){
+                    //Login
+                    moveMainPage(task.result?.user)
+                }else{
+                    ///Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
     }
     fun signinAndSignup(){
-        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
+        auth?.createUserWithEmailAndPassword(binding.emailEdittext.text.toString(), binding.passwordEdittext.text.toString())
                 ?.addOnCompleteListener {
                     task->
                     if (task.isSuccessful){
@@ -35,7 +88,7 @@ class LoginActivity : AppCompatActivity() {
                 }
     }
     fun signinEmail(){
-        auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
+        auth?.signInWithEmailAndPassword(binding.emailEdittext.text.toString(), binding.passwordEdittext.text.toString())
                 ?.addOnCompleteListener {
                     task->
                     if (task.isSuccessful){
